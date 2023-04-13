f70503cde1f556bb7a8787a2a5bf5ea31c6ce130
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-04-13 18:28:27 +0200

    Bumped inputs and reduced supported systems

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         ./firmware
         ./support
       ];
-      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      systems = ["x86_64-linux" "aarch64-linux"];
     });
 
   nixConfig.sandbox = "relaxed";
